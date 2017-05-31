62c686d475dd64fda38b62d099518401a4fb7125
Alexandre Morin <alexandre.morin@maarch.org>
2017-05-31 10:15:43 +0200

    Rename character(e)

--- src/app/character-detail/character-detail.component.spec.ts
+++ src/app/character-detail/character-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CharactereDetailComponent } from './charactere-detail.component';
+import { CharacterDetailComponent } from './character-detail.component';
 
-describe('CharactereDetailComponent', () => {
-  let component: CharactereDetailComponent;
-  let fixture: ComponentFixture<CharactereDetailComponent>;
+describe('CharacterDetailComponent', () => {
+  let component: CharacterDetailComponent;
+  let fixture: ComponentFixture<CharacterDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CharactereDetailComponent ]
+      declarations: [ CharacterDetailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CharactereDetailComponent);
+    fixture = TestBed.createComponent(CharacterDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
