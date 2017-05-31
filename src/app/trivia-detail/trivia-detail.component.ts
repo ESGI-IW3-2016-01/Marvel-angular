255e1ff4df7fd1bfc4a6172fd2e8f368650d096a
Alexandre Morin <alexandre.morin@maarch.org>
2017-05-31 15:22:33 +0200

    trivia service

--- src/app/trivia-detail/trivia-detail.component.ts
+++ src/app/trivia-detail/trivia-detail.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TriviaService } from '../trivia.service';
 
 @Component({
   selector: 'app-trivia-detail',
   templateUrl: './trivia-detail.component.html',
-  styleUrls: ['./trivia-detail.component.css']
+  styleUrls: ['./trivia-detail.component.css'],
+  providers: [TriviaService]
 })
 export class TriviaDetailComponent implements OnInit {
 
-  constructor() { }
+  trivia = this.triviaService.getTrivia();
+  constructor(private triviaService: TriviaService) { }
 
   ngOnInit() {
   }
--- src/app/trivia.service.ts
+++ src/app/trivia.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
-
+import {Trivia} from './trivia';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class TriviaService {
 
-  private triviaUrl = 'https://opentdb.com/api.php?amount=20&category=29';
+  private triviaUrl = 'https://opentdb.com/api.php?amount=10&category=29';
 
-  constructor() { }
+  static handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  getTrivia(): Promise<Trivia[]> {
+
+    return this.http.get(this.triviaUrl)
+      .toPromise()
+      .then((response) => {
+        return response.json().results;
+      })
+      .catch(TriviaService.handleError);
+  }
+
+  constructor(private http: Http) {
+  }
 }
