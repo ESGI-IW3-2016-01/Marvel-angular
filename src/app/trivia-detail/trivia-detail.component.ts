40d2b99b4afd792f65b51312553481f722756763
Alexandre Morin <alexandre.morin@maarch.org>
2017-06-02 11:12:26 +0200

    Ranking trivia

--- src/app/trivia-detail/trivia-detail.component.ts
+++ src/app/trivia-detail/trivia-detail.component.ts
@@ -10,9 +10,51 @@ import { TriviaService } from '../trivia.service';
 export class TriviaDetailComponent implements OnInit {
 
   trivias = this.triviaService.getTrivia();
+  localStorageKey = 'ranking';
+  currentRanking: {};
   constructor(private triviaService: TriviaService) { }
 
   ngOnInit() {
+    this.getAll();
   }
 
+  clicked() {
+    let result = 0;
+    for (let i = 0; i < 10; i++) {
+      const choice = document.querySelector('input[name = "answer' + i + '"]:checked');
+
+      if (!choice) {
+        break;
+      }
+      const correctAnswer =  document.querySelector('input[name = "correct' + i + '"]');
+      if ((<HTMLInputElement>choice).value === (<HTMLInputElement>correctAnswer).value) {
+        result ++;
+      }
+    }
+    const name = ((<HTMLInputElement>document.getElementById('Name')).value);
+
+    if (name) {
+      let ranking = JSON.parse(localStorage.getItem(this.localStorageKey));
+      if (!ranking) {
+        ranking = [];
+      }
+      const player = {
+        'name' : name,
+        'result' : result.toString()
+      };
+      ranking.push(player);
+      localStorage.setItem(this.localStorageKey, JSON.stringify(ranking));
+    }
+    alert(result + ' correct answers');
+    this.getAll();
+    location.reload();
+  }
+
+  getAll() {
+    this.currentRanking = JSON.parse(localStorage['ranking']).sort(this.tri);
+  }
+
+  tri(a, b) {
+  return b.result - a.result;
+  }
 }
